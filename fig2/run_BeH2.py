7c81f0e9adf794b510d2ae939ccf84adbc8843ae
Thitiwuth Chaiyatho <atom@atom-mac-pro.local>
2026-02-02 22:04:24 +0700

    Add python scripts for fig-2

---
+++ fig2/run_BeH2.py
@@ -0,0 +1,29 @@
+from vqengine import *
+
+Name = "BeH2"
+dt = 0.1
+total_t = 1.0
+
+# Define geometry
+geometry = [("Be", (0., 0., 0.)), ("H", (0., 0., 0.75)), ("H", (0., 0., -0.75))]
+
+
+# Initialize Molecule
+mol = Molecule(geometry, multiplicity=1, freeze=2)
+H, hf = mol.get_hamiltonian(transform='jw')
+
+# Generate Pools
+qubit_pool, qubit_v_pool = generate_qubit_pool(mol.n_qubits, mol.n_electrons)
+
+# Run
+qubit_E, qubit_thetas, qubit_gates, qubit_gates_t, qubit_L2_t, qubit_variance_t = driver.avqite(
+    qubit_pool, qubit_v_pool, dt=dt, grad_tol=5e-4, t=total_t
+)
+
+# Post-processing
+qubit_errors = [abs(e - mol.fci_energy) for e in qubit_E]
+t_vals = [dt * n for n in range(len(qubit_E))]
+qubit_L2, qubit_t_L2 = get_t_L2(qubit_L2_t)
+qubit_variance, _ = get_t_variance(qubit_variance_t)
+qubit_t_L2 = [m*dt for m in qubit_t_L2]
+qubit_ncnot, qubit_nparams = count_cn(qubit_gates_t)
